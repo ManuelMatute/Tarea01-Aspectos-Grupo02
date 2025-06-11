6dabb05a7c3f2bd27ebe415af7c9155df32e307c
wentlandarias <wenraria@espol.edu.ec>
2025-06-10 20:58:31 -0500

    cambios en sujeto
    cambios hechos en sujeto

--- src/Observer/Sujeto.java
+++ src/Observer/Sujeto.java
@@ -1,5 +1,21 @@
 package Observer;
+ import java.util.ArrayList;
+import java.util.List;
 
 public class Sujeto {
+   
 
+
+    private final List<Observador> observadores = new ArrayList<>();
+
+    public void agregarObservador(Observador o) {
+        observadores.add(o);
+    }
+
+    public void notificar(Color color) {
+        for (Observador o : observadores) {
+            o.actualizar(color);
+        }
+    }
 }
+
